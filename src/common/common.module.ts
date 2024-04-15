25af1ffb5c1d63559d435edad3aa8968c7adbd50
Satria Bahari <satriaabaharii@gmail.com>
2024-04-15 22:40:18 +0700

    refactor: create common module

---
+++ src/common/common.module.ts
@@ -0,0 +1,19 @@
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { ValidationService } from './validation.service';
+import { APP_FILTER } from '@nestjs/core';
+import { ErrorFilter } from './error.filter';
+
+@Global()
+@Module({
+  providers: [
+    PrismaService,
+    ValidationService,
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter,
+    },
+  ],
+  exports: [PrismaService, ValidationService],
+})
+export class CommonModule {}
