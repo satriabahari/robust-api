685c008334851a0e22a7a71a913ed6fe3e38cc04
Satria Bahari <satriaabaharii@gmail.com>
2024-04-15 22:31:18 +0700

    refactor: add some class on user model

---
+++ src/model/user.model.ts
@@ -0,0 +1,16 @@
+export class RegisterUserRequest {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export class UserResponse {
+  username: string;
+  email: string;
+  token?: string;
+}
+
+export class LoginUserRequest {
+  email: string;
+  password: string;
+}
