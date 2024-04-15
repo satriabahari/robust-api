77a9c1b3d7626fb339244e02faee8d4dc1c3e472
Satria Bahari <satriaabaharii@gmail.com>
2024-04-15 22:33:53 +0700

    test: add test register and login

---
+++ test/user.spec.ts
@@ -0,0 +1,102 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+import { TestService } from './test.service';
+import { TestModule } from './test.module';
+
+describe('UserController', () => {
+  let app: INestApplication;
+  let testService: TestService;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, TestModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+
+    testService = app.get(TestService);
+  });
+
+  describe('POST /api/users', () => {
+    beforeEach(async () => {
+      await testService.deleteAll();
+    });
+
+    it('should be rejected if request is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: '',
+          email: '',
+          password: '',
+        });
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it('should be able to register', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: 'test',
+          email: 'test@gmail.com',
+          password: 'test',
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.data.username).toBe('test');
+      expect(response.body.data.email).toBe('test@gmail.com');
+    });
+
+    it('should be rejected if email already exists', async () => {
+      await testService.createUser();
+      const response = await request(app.getHttpServer())
+        .post('/api/users')
+        .send({
+          username: 'test',
+          email: 'test@gmail.com',
+          password: 'test',
+        });
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+  });
+
+  describe('POST /api/users/login', () => {
+    beforeEach(async () => {
+      await testService.deleteAll();
+      await testService.createUser();
+    });
+
+    it('should be rejected if request is invalid', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users/login')
+        .send({
+          email: '',
+          password: '',
+        });
+
+      expect(response.status).toBe(400);
+      expect(response.body.errors).toBeDefined();
+    });
+
+    it('should be able to login', async () => {
+      const response = await request(app.getHttpServer())
+        .post('/api/users/login')
+        .send({
+          email: 'test@gmail.com',
+          password: 'test',
+        });
+
+      expect(response.status).toBe(200);
+      expect(response.body.data.username).toBe('test');
+      expect(response.body.data.email).toBe('test@gmail.com');
+      expect(response.body.data.token).toBeDefined();
+    });
+  });
+});
